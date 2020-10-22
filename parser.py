60aba1a2d92797fae90927283071d9931754e631
karelinaas <karelinaas95@gmail.com>
2020-10-22 21:37:10 +0300

    strip tags (in city)

--- parser.py
+++ parser.py
@@ -1,3 +1,4 @@
+import re
 import sys
 
 import AdvancedHTMLParser
@@ -23,19 +24,29 @@ if len(sys.argv) > 1:
         for i, vac_tag in enumerate(vacancies):
             sub_tags = vac_tag.getAllChildNodes()
 
+            vacancy_info = {
+                'link': '',
+                'name': '',
+                'salary': '',
+                'city': '',
+            }
+
             for tag in sub_tags:
+                if vacancy_info['link'] and vacancy_info['name'] and vacancy_info['salary'] and vacancy_info['city']:
+                    break
+
                 classes = str(tag.classNames).strip()
 
                 if classes == 'bloko-link HH-LinkModifier':
-                    print(i, 'Ссылка:', tag.href)
-                    print('Название:', tag.innerText)
+                    vacancy_info['link'] = tag.href
+                    vacancy_info['name'] = tag.innerText
                 elif classes == 'bloko-section-header-3 bloko-section-header-3_lite' and tag.innerText:
-                    print('ЗП:', tag.innerText)
+                    vacancy_info['salary'] = tag.innerText
                 elif classes == 'vacancy-serp-item__meta-info' and 'vacancy-serp__vacancy-address' in tag.innerHTML:
-                    print('Город:', tag.innerHTML)
-                    print(tag.innerText)
+                    vacancy_info['city'] = re.sub('<[^<]+?>', '', tag.innerHTML)
 
-            print('------------------ page', page)
+            print(vacancy_info)
+        print('------------------ page', page)
         page += 1
 else:
     exit('Не указан обязательный параметр - URL')
